06731bce089395789f4a175edb1a76c8b45acf43
shreya <shreyan20@iitk.ac.in>
2023-06-14 15:40:10 +0530

    first commit

--- src/function-factory.ts
+++ src/function-factory.ts
@@ -1,9 +1,11 @@
 import function_1 from './functions/function_1/index';
 import function_2 from './functions/function_2/index';
+import shreya_function from './functions/function_2/index';
 
 export const functionFactory = {
   function_1,
   function_2,
+  shreya_function,
 } as const;
 
 export type FunctionFactoryType = keyof typeof functionFactory;
