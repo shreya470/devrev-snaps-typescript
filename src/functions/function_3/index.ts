f454051373abfb3a2eace718d1435131a00be76e
shreya <shreyan20@iitk.ac.in>
2023-06-14 15:55:22 +0530

    fourth commit

---
+++ src/functions/function_3/index.ts
@@ -0,0 +1,10 @@
+export const run = async (events: any[]) => {
+    /*
+    Put your code here and remove the log below
+    */
+  
+    console.info('events', events);
+  };
+  
+  export default run;
+  
